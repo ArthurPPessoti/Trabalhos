11411a74e5215acfdd320d2665a322313d57d242
Arthur Prates Pessoti <159321443+ArthurPPessoti@users.noreply.github.com>
2025-05-31 18:36:16 -0300

    Java
    Pasta Java de desafios

---
+++ Java/ExercicioGrupoEmpresarial/src/main/java/com/mycompany/exerciciogrupoempresarial/Investimento.java
@@ -0,0 +1,27 @@
+package com.mycompany.exerciciogrupoempresarial;
+
+public class Investimento {
+    private int id;
+    private double valor;
+    private Administradora administradora;
+    
+    public Investimento(int id, double valor, Administradora administradora){
+        this.id = id;
+        this.valor = valor;
+        this.administradora = administradora;
+    }
+
+    public double getValor() {
+        return valor;
+    }   
+    
+    public int getId(){
+        return id;
+    }
+    
+    @Override           
+    public String toString(){
+        return "ID " + this.id + "\n" +
+                "Valor: R$" + this.valor + "\n";
+    }
+}
