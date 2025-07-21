209d56c57993ba5547b2a99a6cdae2d299060115
Arthur Prates Pessoti <159321443+ArthurPPessoti@users.noreply.github.com>
2025-07-21 14:44:03 -0300

    Contador PYTHON
    Desafio: Contador
    Descrição: Imprima os números de 1 a 10 utilizando um laço de repetição.

---
+++ Python/Contador/contador1.py
@@ -0,0 +1,8 @@
+"""
+Desafio: Contador
+Descrição: Imprima os números de 1 a 10 utilizando um laço de repetição.
+"""
+numero = int(input("Quantos números voê quer que sejam impressos? "))
+
+for i in range(numero):
+    print(i+1)
